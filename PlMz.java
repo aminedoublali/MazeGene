b7799c23b313ba6c1ce2f888327c5c22a817ff39
PntsKS02 <tadami.kiha40.2085@gmial.com>
2023-04-27 14:16:28 +0900

    Fujisaki_update

--- PlMz.java
+++ PlMz.java
@@ -1,30 +1,69 @@
+package meiro;
 import java.util.Scanner;
 
-public class PlMz(start,goal,maze,PL){
-
-    public void Koma_Distination(){
-    //コマの移動入力はWASDのキーボードを使っておこなう　W = UP,S = DOWN,A = LEFT,D = RIGHT
-    System.out.println("移動したい方向を入力してください");
-    System.out.println("W -> 上　S -> 下　A -> 左　D -> 右");
-    Scanner GO = new Scanner(System.in); //進行方向を聞く
-    String Distination = GO.nextString();
-    switch (Distination) {
-        case "W":
-        
-            break;
-  
-        case "S":
-            break;
-
-        case "A":
-            break;
-
-        case "D":
-            break;
-    
-        default:
-            break;
-    } 
-    } 
+public class PlMz {
+	
+	
+	//コマの移動入力の受付
+	void ido(int x, int y , int [][] Mz) { //x, yは
+		System.out.println("移動したい方向を入力");
+		System.out.println("w:↑ d:→ s:↓ a");//移動の入力はw,a,s,dで行う
+		
+		//矢印キーの受け取り
+		Scanner scanner = new Scanner(System.in);
+		String n = scanner.nextLine();
+		System.out.println(n);
+		
+		//移動
+		switch(n) {
+		case "w":
+				if(Mz[x][y + 1] != 1) {
+					y++;
+				}
+				if(Mz[x][y + 1] == 1) {//進めなかった場合
+					System.out.println("進むことができません");
+				}
+				break;
+		case "d":
+				if(Mz[x + 1][y] != 1) {
+					x++;
+				}
+				if(Mz[x + 1][y] == 1) {//進めなかった場合
+					System.out.println("進むことができません");
+				}
+				break;
+		case "s":
+				if(Mz[x][y - 1] != 1) {
+					y--;
+				}
+				if(Mz[x][y - 1] == 1) {//進めなかった場合
+					System.out.println("進むことができません");
+				}
+				break;
+		case "a":
+				if(Mz[x - 1][y] != 1) {
+					x --;
+				}
+				if(Mz[x - 1][y] == 1) {//進めなかった場合
+					System.out.println("進むことができません");
+				}
+				break;
+		default:
+			System.out.println("入力内容が間違っています");
+			break;
+		}
+		
+		//移動可能かどうかの判定
+		//startの一つ外側も移動不可にしておく
+		
+	}
+	
+	//ゴール座標と一致したかどうかの判定
+	void finish(int x, int y, int goalX, int goalY) {
+		
+		if( x == goalX && y == goalY) {
+			System.out.println("ゴールしました");
+		}
+	}
 
 }
