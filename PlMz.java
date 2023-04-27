e85d5f8c82a28a1bb54a9974561c7ec4a727c692
PntsKS02 <tadami.kiha40.2085@gmial.com>
2023-04-27 14:09:41 +0900

    Master

---
+++ PlMz.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PlMz(start,goal,maze,PL){
+
+    public void Koma_Distination(){
+    //コマの移動入力はWASDのキーボードを使っておこなう　W = UP,S = DOWN,A = LEFT,D = RIGHT
+    System.out.println("移動したい方向を入力してください");
+    System.out.println("W -> 上　S -> 下　A -> 左　D -> 右");
+    Scanner GO = new Scanner(System.in); //進行方向を聞く
+    String Distination = GO.nextString();
+    switch (Distination) {
+        case "W":
+        
+            break;
+  
+        case "S":
+            break;
+
+        case "A":
+            break;
+
+        case "D":
+            break;
+    
+        default:
+            break;
+    } 
+    } 
+
+}
