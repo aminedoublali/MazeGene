65bb04b1d15c08eebc1f57e607ebf68c9ed4aa2c
DESKTOP-RCJL5RC\DOUBLALI AMINE <amine.doublali@g.dareamcareer.co.jp>
2023-04-27 14:19:31 +0900

    エラーなおした

--- PlMz.java
+++ PlMz.java
@@ -1,67 +1,66 @@
-package meiro;
+
 import java.util.Scanner;
 
 public class PlMz {
-	
-	
-	//コマの移動入力の受付
-	void ido(int x, int y , int [][] Mz) { //x, yは
+
+	// コマの移動入力の受付
+	void ido(int x, int y, int[][] Mz) { // x, yは
 		System.out.println("移動したい方向を入力");
-		System.out.println("w:↑ d:→ s:↓ a");//移動の入力はw,a,s,dで行う
-		
-		//矢印キーの受け取り
+		System.out.println("w:↑ d:→ s:↓ a");// 移動の入力はw,a,s,dで行う
+
+		// 矢印キーの受け取り
 		Scanner scanner = new Scanner(System.in);
 		String n = scanner.nextLine();
 		System.out.println(n);
-		
-		//移動
-		switch(n) {
-		case "w":
-				if(Mz[x][y + 1] != 1) {
+
+		// 移動
+		switch (n) {
+			case "w":
+				if (Mz[x][y + 1] != 1) {
 					y++;
 				}
-				if(Mz[x][y + 1] == 1) {//進めなかった場合
+				if (Mz[x][y + 1] == 1) {// 進めなかった場合
 					System.out.println("進むことができません");
 				}
 				break;
-		case "d":
-				if(Mz[x + 1][y] != 1) {
+			case "d":
+				if (Mz[x + 1][y] != 1) {
 					x++;
 				}
-				if(Mz[x + 1][y] == 1) {//進めなかった場合
+				if (Mz[x + 1][y] == 1) {// 進めなかった場合
 					System.out.println("進むことができません");
 				}
 				break;
-		case "s":
-				if(Mz[x][y - 1] != 1) {
+			case "s":
+				if (Mz[x][y - 1] != 1) {
 					y--;
 				}
-				if(Mz[x][y - 1] == 1) {//進めなかった場合
+				if (Mz[x][y - 1] == 1) {// 進めなかった場合
 					System.out.println("進むことができません");
 				}
 				break;
-		case "a":
-				if(Mz[x - 1][y] != 1) {
-					x --;
+			case "a":
+				if (Mz[x - 1][y] != 1) {
+					x--;
 				}
-				if(Mz[x - 1][y] == 1) {//進めなかった場合
+				if (Mz[x - 1][y] == 1) {// 進めなかった場合
 					System.out.println("進むことができません");
 				}
 				break;
-		default:
-			System.out.println("入力内容が間違っています");
-			break;
+			default:
+				System.out.println("入力内容が間違っています");
+				break;
 		}
-		
-		//移動可能かどうかの判定
-		//startの一つ外側も移動不可にしておく
-		
+
+		// 移動可能かどうかの判定
+		// startの一つ外側も移動不可にしておく
+
 	}
-	
-	//ゴール座標と一致したかどうかの判定
+
+	// ゴール座標と一致したかどうかの判定
 	void finish(int x, int y, int goalX, int goalY) {
-		
-		if( x == goalX && y == goalY) {
+
+		if (x == goalX && y == goalY) {
 			System.out.println("ゴールしました");
 		}
 	}
