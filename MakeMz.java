220e1b41872f201932b34ab0482988ac6d2f0da2
DC-RF1026 <ryosuke.fukuda@g.dreamcareer.co.jp>
2023-04-27 14:47:56 +0900

    MakeMz Update

--- MakeMz.java
+++ MakeMz.java
@@ -1,5 +1,12 @@
-package maze;
+import java.util.*;
 
 public class MakeMz{
-  
+  int leng = 16;
+  ArrayList<String>[][] Maze = new ArrayList[leng][leng];
+  // 
+  for(int i = 0: i < leng; i++){
+    for (int g = 0; g < leng; g++){
+
+    }
+  }
 }
