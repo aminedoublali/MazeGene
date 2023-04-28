54ea044cbad8eec5252a8a14be7ba8c212008c48
DESKTOP-RCJL5RC\DOUBLALI AMINE <amine.doublali@g.dareamcareer.co.jp>
2023-04-28 12:31:32 +0900

    hensyuu

--- Main.java
+++ Main.java
@@ -4,28 +4,30 @@ import java.util.*;
 public class Main extends Mazetextsystem {
 
     public static void main(String[] args) {
-        //TODO: ここ閉じたほうが良くない？
+        // TODO: ここ閉じたほうが良くない？
         Scanner User_input = new Scanner(System.in);
         // ゲーム開始までのユーザー操作
         System.out.println("迷路ゲーム");
         System.out.println("ゲームを始めますか？ y");
+        // 下難易度マップ呼び出し頼む！！！！↓
+        MakeMz MakeMz = new MakeMz();
+        MakeMz.ChooseMz();
         // Gametextメソッドを呼び出し処理させています
         // yと入力されたらゲームをはじめy以外はゲームを終了する。
         Gametext game = new Gametext();
-        //textの表示と迷路の描画
-        //TODO:迷路の描画は個別にメソッド化する ← 迷路の描画は煩雑でないため毎時コードを書く
-        //↑やっぱりメソッド化したほうが良いかも？
-        
+        // textの表示と迷路の描画
+        // TODO:迷路の描画は個別にメソッド化する ← 迷路の描画は煩雑でないため毎時コードを書く
+        // ↑やっぱりメソッド化したほうが良いかも？
 
-        //迷路の地図を描画するために地図を与える
-        game.gametextsystem(); 
+        // 迷路の地図を描画するために地図を与える
+        game.gametextsystem();
 
         //
-        //while文 →ゴールしない限りにすれば解決
+        // while文 →ゴールしない限りにすれば解決
         PlMz PL = new PlMz();
         PL.ido();
 
-        //TODO: クリアした演出考える gameClear();
+        // TODO: クリアした演出考える gameClear();
         game.gameClear();
 
     }
